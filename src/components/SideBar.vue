d21aaf62c48245d168d0a0d1b3ed16a7ce8ac06f
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-26 18:01:03 +0200

    feat: :sparkles: add the SideBar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <!-- v-bind directive (:) is used to bind variables to attributes. -->
+      <v-list-item
+        :prepend-avatar="avatarPic"
+        subtitle="To use in your projects"
+        title="Image Gallery"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+        @click="selectColorOption(true)"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-dialpad"
+        title="Black & White Gallery"
+        value="black-and-white"
+        @click="selectColorOption(false)"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits(["selectColorOption"]);
+
+const selectColorOption = (isColor: boolean) => {
+  emits("selectColorOption", isColor);
+};
+
+const avatarPic = import.meta.env.VITE_APP_AVATAR_PIC || "";
+</script>
+
+<style scoped>
+.mdi-dialpad {
+  background-color: blueviolet;
+  color: blueviolet;
+}
+</style>
