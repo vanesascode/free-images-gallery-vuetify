eb5f51d7fab2255fff92983d89db9644f5ffe48a
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-26 18:24:28 +0200

    refactor: :recycle: remove HelloWorld component from components.d.ts file

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     ImageGallery: typeof import('./src/components/ImageGallery.vue')['default']
     InstructionsPopup: typeof import('./src/components/InstructionsPopup.vue')['default']
     SideBar: typeof import('./src/components/SideBar.vue')['default']
