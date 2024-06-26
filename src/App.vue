ec7093e4d912d2948b0e6e5655fd5d2ebc61a93c
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-26 18:00:19 +0200

    feat: :sparkles: add the main layout of the app in the App component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <SideBar @selectColorOption="onSelectColorOption" />
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor" />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+// REF creates a REACTIVE reference to a value.
+import { ref } from "vue";
+
+const isWithColor = ref(true);
+
+const onSelectColorOption = (isColor: boolean) => {
+  isWithColor.value = isColor;
+};
+</script>
